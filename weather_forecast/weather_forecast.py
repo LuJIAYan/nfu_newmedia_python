a4254ac8c8ccc80f9766fb18e73da3a391e87fa5
LuJIAYan <lujiayan123@126.com>
2017-06-03 01:22:40 +0800

    Update weather_forecast.py

--- weather_forecast/weather_forecast.py
+++ weather_forecast/weather_forecast.py
@@ -26,9 +26,17 @@ def get_weather(city_name):
         response_life = urllib.request.urlopen(url_life)
         weather_html_life = response_life.read()
         json_data_life = json.loads(weather_html_life)
-
-
+        
         data_life = json_data_life['HeWeather5'][0]
+        
+         #未来三天天气预报api
+        url_daily_forecast = ('https://free-api.heweather.com/v5/dailyforecast?city=CN{code}&key=50246ea62fbd405c9315e51d2a1d29c0'.format(code=city_code))
+        response_daily_forecast = urllib.request.urlopen(url_daily_forecast)
+        weather_html_daily_forecast = response_daily_forecast.read()
+        json_data_daily_forecast = json.loads(weather_html_daily_forecast)
+        
+        data_life = json_data_life['HeWeather5'][0]
+        date=data['daily_forecast'][0]['date']
         '''
         #空气状况
         air_brf = data_life['suggestion']['air']['brf']
